f2e6ec39083fc459a49d0f205194037a09eeea98
Geoff Taylor <geoff@axistaylor.com>
2021-03-31 00:25:04 -0400

    WP-PHPUnit support added and tested.

--- src/TestCase/WPGraphQLUnitTestCase.php
+++ src/TestCase/WPGraphQLUnitTestCase.php
@@ -1,32 +1,27 @@
 <?php
 /**
- * WPGraphQL test case (For Codeception)
+ * WPGraphQL test case (for PHPUnit)
  *
  * For testing WPGraphQL responses.
- * @since 1.0.0
+ * @since 1.1.0
  * @package Tests\WPGraphQL\TestCase
  */
-
 namespace Tests\WPGraphQL\TestCase;
 
-/**
- * WPGraphQLTestCase class.
- */
-class WPGraphQLTestCase extends \Codeception\TestCase\WPTestCase {
+abstract class WPGraphQLUnitTestCase extends \WP_UnitTestCase {
+    use WPGraphQLTestCommon;
 
-	use WPGraphQLTestCommon;
-
-	/**
+    /**
 	 * Console logging function.
 	 *
 	 * Use --debug flag to view in console.
 	 */
 	protected function logData( $data ) {
 		if ( is_array( $data ) || is_object( $data ) ) {
-			\codecept_debug( json_encode( $data, JSON_PRETTY_PRINT ) );
+			fwrite( STDOUT, json_encode( $data, JSON_PRETTY_PRINT ) );
 			return;
 		}
 
-		\codecept_debug( $data );
+		fwrite( STDOUT, $data );
 	}
 }
